19653d7906bbb0adc6eb64c65b32bb1161e3c669
steph <stm_artin@yahoo.fr>
2025-08-10 16:51:06 +0200

    mfe1 s affiche bien dans host_app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-     {
+    {
         path: '',
         loadComponent: () =>
             loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent),
